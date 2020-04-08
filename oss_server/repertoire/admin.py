a82f5e1c5fb0babd794a44e25c00a400ecc7788d
Anton Mrosek <info@anjomro.de>
2020-04-08 15:42:47 +0200

    Added models to admin interface
    Took 25 seconds

--- oss_server/repertoire/admin.py
+++ oss_server/repertoire/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from repertoire.models import Tag, Area, Playlist, Artist, Album, Track
 # Register your models here.
-admin.register(Tag)
-admin.register(Area)
-admin.register(Playlist)
-admin.register(Artist)
-admin.register(Album)
-admin.register(Track)
+admin.site.register(Tag)
+admin.site.register(Area)
+admin.site.register(Playlist)
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Track)
