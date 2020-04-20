03f3765eb4a59d3d4d4a4abc8737710f9fa50850
Anton Mrosek <info@anjomro.de>
2020-04-20 10:22:02 +0200

    Added AreaResource in Api v1
    Took 3 minutes

--- oss_server/repertoire/api/v1.py
+++ oss_server/repertoire/api/v1.py
@@ -4,6 +4,7 @@ from tastypie.api import Api
 v1_api = Api(api_name="v1")
 v1_api.register(TagResource())
 v1_api.register(ArtistResource())
+v1_api.register(AreaResource())
 v1_api.register(AlbumResource())
 v1_api.register(TrackResource())
 v1_api.register(SongResource())
