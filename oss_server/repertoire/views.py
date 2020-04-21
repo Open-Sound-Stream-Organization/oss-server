853a23554a1ba63151067c0b377beaf300f43b60
Anton Mrosek <info@anjomro.de>
2020-04-21 20:08:11 +0200

    Avoid HTTP Error 500 when no file was found
    Took 17 minutes

--- oss_server/repertoire/views.py
+++ oss_server/repertoire/views.py
@@ -1,11 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import FileResponse, HttpResponseForbidden, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth.models import User
-from repertoire.models import Track
-from repertoire.api.ApiKey import ApiKey
 
-def track_file(request, pk:int):
+from repertoire.api.ApiKey import ApiKey
+from repertoire.models import Track
+
+
+def track_file(request, pk: int):
     '''
     Function to serve track files to authenticated users
     :param request: HTTP-Request object
@@ -20,16 +21,17 @@ def track_file(request, pk:int):
             return HttpResponseForbidden("<b>Forbidden:</b> Invalid API-Key!")
         request.user = api_key.user
         track = get_object_or_404(Track, id=pk, user=api_key.user)
-        if track.audio is not None:
+        try:
             return FileResponse(track.audio.open("rb"))
-        else:
-            return HttpResponseNotFound("<b>Not found:</b> For the requested track no audio file was saved on the server")
+        except ValueError:
+            return HttpResponseNotFound(
+                "<b>Not found:</b> For the requested track no audio file was saved on the server")
     else:
         if request.user.is_authenticated:
             track = get_object_or_404(Track, id=pk, user=request.user)
-            if track.audio is not None:
+            try:
                 return FileResponse(track.audio.open("rb"))
-            else:
+            except ValueError:
                 return HttpResponseNotFound(
                     "<b>Not found:</b> For the requested track no audio file was saved on the server")
         return HttpResponseForbidden("<b>Forbidden:</b> Authorized access with an API-Key only!")
